97f8655d214bffc075d2c3ab1177773e13b63ef4
Sugandha <raosugandha12@gmail.com>
2023-02-07 05:12:04 +0000

    updated W12 form with species component

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import W12MHeader from './W12MHeader';
+import SpeciesName from './SpeciesName';
+
+
 
 const W12MForm = () => {
+	const [speciesName, setSpeciesName] = useState<string>('humans')
+
+
+
+
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
+			<SpeciesName speciesName={speciesName} onChangeSpeciesName={function (e: ChangeEvent<HTMLInputElement>): void {
+				throw new Error('Function not implemented.');
+			} }/>
+			
 		</section>
 	);
-};
+}
 
-export default W12MForm;
+export default W12MForm
