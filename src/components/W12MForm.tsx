316c958dc275c582fc3d22eb96577f50fe874d1a
Sugandha <raosugandha12@gmail.com>
2023-02-17 20:57:44 +0000

    Created all components

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,24 +1,13 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import W12MHeader from './W12MHeader';
-import SpeciesName from './SpeciesName';
-
-
 
 const W12MForm = () => {
-	const [speciesName, setSpeciesName] = useState<string>('humans')
-
-
-
-
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			<SpeciesName speciesName={speciesName} onChangeSpeciesName={function (e: ChangeEvent<HTMLInputElement>): void {
-				throw new Error('Function not implemented.');
-			} }/>
-			
+			{/* REST OF FORM GOES HERE */}
 		</section>
 	);
-}
+};
 
-export default W12MForm
+export default W12MForm;
